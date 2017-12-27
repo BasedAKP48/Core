8a3ceb4db982c5d4d2bea8620af0dc689a5808d2
feildmaster <feildmaster@users.noreply.github.com>
2017-12-27 14:02:28 -0600

    chore: Counters only run on create

--- functions/counter.js
+++ functions/counter.js
@@ -5,15 +5,10 @@ const admin = require('firebase-admin');
 const rootRef = admin.database().ref();
 
 function incrementCounter(e) {
-  // Messages only count if they are new, and aren't removed.
-  if (e.data.previous.exists() || !e.data.exists()) {
-    return;
-  }
-
   // increment total message count inside a transaction to be sure we increment properly.
   return rootRef.child('totalMessageCount').transaction((count) => {
     return (count || 0) + 1;
   }).then(()=>{}); // The then() call is to work around a firebase-admin bug.
 };
 
-module.exports = functions.database.ref('/messages/{pushId}').onWrite(incrementCounter);
+module.exports = functions.database.ref('/messages/{pushId}').onCreate(incrementCounter);
