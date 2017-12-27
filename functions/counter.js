b37da920a255e29b9bdc83c76dc9f8bed772f2f3
Austin Peterson <austin@akpwebdesign.com>
2017-12-27 16:15:11 -0500

    I think this terrible hack would work...

--- functions/counter.js
+++ functions/counter.js
@@ -1,19 +1,19 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
-// Reference to the root of our database, for convenience.
-const rootRef = admin.database().ref();
+module.exports = (admin) => {
+  // Reference to the root of our database, for convenience.
+  const rootRef = admin.database().ref();
 
-function incrementCounter(e) {
-  // Messages only count if they are new, and aren't removed.
-  if (e.data.previous.exists() || !e.data.exists()) {
-    return;
-  }
+  const incrementCounter = (e) => {
+    // Messages only count if they are new, and aren't removed.
+    if (e.data.previous.exists() || !e.data.exists()) {
+      return;
+    }
 
-  // increment total message count inside a transaction to be sure we increment properly.
-  return rootRef.child('totalMessageCount').transaction((count) => {
-    return (count || 0) + 1;
-  }).then(()=>{}); // The then() call is to work around a firebase-admin bug.
+    // increment total message count inside a transaction to be sure we increment properly.
+    return rootRef.child('totalMessageCount').transaction((count) => {
+      return (count || 0) + 1;
+    }).then(()=>{}); // The then() call is to work around a firebase-admin bug.
+  };
+  return functions.database.ref('/messages/{pushId}').onWrite(incrementCounter);
 };
-
-module.exports = functions.database.ref('/messages/{pushId}').onWrite(incrementCounter);
--- functions/index.js
+++ functions/index.js
@@ -4,5 +4,5 @@ const admin = require('firebase-admin');
 // Initialize the Firebase app using firebase-functions built-in config object.
 admin.initializeApp(functions.config().firebase);
 
-exports.incrementMessageCounter = require('./counter.js');
+exports.incrementMessageCounter = require('./counter.js')(admin);
 exports.github = require('./github.js');
