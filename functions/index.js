826c8aeef641e04bdd2266ae7f64d7cf0df3afe1
feildmaster <feildmaster@users.noreply.github.com>
2017-12-30 14:25:30 -0600

    feat: Slightly improve runtime of functions

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,14 @@ const admin = require('firebase-admin');
 // Initialize the Firebase app using firebase-functions built-in config object.
 admin.initializeApp(functions.config().firebase);
 
-//exports.processMessage = require('./process.js'); // Uncomment for a passive listener
-exports.incrementMessageCounter = require('./counter.js');
-exports.github = require('./github.js');
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'processMessage') {
+  //exports.processMessage = require('./process.js'); // Uncomment for a passive listener
+}
+
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'incrementMessageCounter') {
+  exports.incrementMessageCounter = require('./counter.js');
+}
+
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'github') {
+  exports.github = require('./github.js');
+}
