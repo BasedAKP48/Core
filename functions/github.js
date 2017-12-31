51fd5eaa421a3cdb8c8ef45d32716f3d65b10670
Austin Peterson <austin@akpwebdesign.com>
2017-12-30 23:59:48 -0500

    fix(fns): remove unnecessary bluebird depend

--- functions/github.js
+++ functions/github.js
@@ -1,6 +1,5 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const Promise = require('bluebird');
 const crypto = require('crypto');
 
 // Reference to the root of our database, for convenience.
