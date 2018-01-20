5b347f27b790ab095d6c34699e65bbb8960d3aee
feildmaster <feildmaster@users.noreply.github.com>
2018-01-19 23:11:04 -0600

    chore: Add eslint support

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require("./serviceAccount.json");
+const serviceAccount = require('./serviceAccount.json');
 const { initialize, PresenceSystem, getCID } = require('@basedakp48/plugin-utils');
 const pkg = require('./package.json');
 
@@ -16,7 +16,7 @@ presenceSystem.initialize({
   cid,
   pkg,
   instanceName: 'Core',
-  listenMode: 'core'
+  listenMode: 'core',
 });
 
 presenceSystem.on('connect', () => {
@@ -51,32 +51,27 @@ const ALLOWED_MESSAGE_FIELDS = [
 
 /**
  * Messages must have certain properties before they can be acted upon by the core and plugins.
- * This function ensures that any incoming messages include all required properties before being moved to the messages
- * queue.
+ * This function ensures that any incoming messages include all required properties before being
+ * moved to the messages queue.
  */
 function processMessage(e) {
-  let msg = e.val();
-
-  console.log(msg);
+  const msg = e.val();
 
   // if we don't have a message, just return.
-  if(!msg) { return; }
+  if (!msg) { return; }
 
   // for each required field, make sure the message has the field.
   for (let i = 0; i < REQUIRED_MESSAGE_FIELDS.length; i++) {
-    let field = REQUIRED_MESSAGE_FIELDS[i];
-    if (!hasOne(msg, field)) {
-      return e.ref.remove(); // if not, remove the raw message from the queue, as it is malformed.
-    }
+    const field = REQUIRED_MESSAGE_FIELDS[i];
+    // if not, remove the raw message from the queue, as it is malformed.
+    if (!hasOne(msg, field)) return e.ref.remove();
   }
 
   // for each field in the message, verify that the key is allowed.
   // if the key is not allowed, it is extraneous, and should be removed.
-  for (let k in msg) {
-    if (msg.hasOwnProperty(k) && !ALLOWED_MESSAGE_FIELDS.includes(k)) {
-      delete msg[k];
-    }
-  }
+  Object.keys(msg).forEach((k) => {
+    if (Object.hasOwnProperty.call(msg, k) && !ALLOWED_MESSAGE_FIELDS.includes(k)) delete msg[k];
+  });
 
   // add the time received, if the providing plugin did not populate it.
   if (!msg.timeReceived) {
@@ -84,7 +79,7 @@ function processMessage(e) {
   }
 
   // Is this an outgoing message?
-  let outgoing = msg.hasOwnProperty('target');
+  const outgoing = Object.hasOwnProperty.call(msg, 'target');
   if (outgoing) {
     msg.cid = msg.target;
     delete msg.target;
@@ -96,10 +91,8 @@ function processMessage(e) {
 
   // Is this an internal message?
   if (msg.type.toLowerCase() === 'internal' || msg.type.toLowerCase() === 'akpacket') {
-    msg.type = 'internal';  // force a standardized type
-    return rootRef.child(`clients/${msg.cid}`).push(msg).then(() => {
-      return e.ref.remove();
-    });
+    msg.type = 'internal'; // force a standardized type
+    return rootRef.child(`clients/${msg.cid}`).push(msg).then(() => e.ref.remove());
   }
 
   // push the message into the messages queue and remove it from the raw messages queue.
@@ -107,18 +100,17 @@ function processMessage(e) {
     if (outgoing) {
       rootRef.child(`clients/${msg.cid}`).push(msg);
     }
-  }).then(() => {
-    return e.ref.remove();
-  });
+  }).then(() => e.ref.remove());
 }
 
 function hasOne(object, array) {
   if (!Array.isArray(array)) array = [array];
   let count = 0;
   for (let i = 0; i < array.length; i++) {
-    let key = array[i];
-    if (object.hasOwnProperty(key) && object[key] !== null && object[key] !== undefined) {
-      count++;
+    const key = array[i];
+    const objectHasKey = Object.hasOwnProperty.call(object, key);
+    if (objectHasKey && object[key] !== null && object[key] !== undefined) {
+      count += 1;
     }
   }
   return count === 1;
